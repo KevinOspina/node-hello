4c2094d2dfdf9d2f7b168070ddb48c2fcf56b117
Kevin Ospina Cañaveral <36991694+KevinOspina@users.noreply.github.com>
2021-08-16 14:43:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
   res.end(msg);
-  server.timeout = 2000;
+  
 });
 
 server.listen(port, () => {
+  server.timeout = 2000;
   console.log(`Server running on http://localhost:${port}/`);
 });
